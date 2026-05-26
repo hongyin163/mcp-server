80f945a4c3433cf42cb1bab1d647c0acb0ee3fd2
hongyin163 <hongyin163@163.com>
2026-05-26 13:11:53 +0800

    update

--- api/index.js
+++ api/index.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const serverless = require('serverless-http');
 const { app, loadConfig } = require('../src/app');
 const config = require('../src/config');
 
@@ -10,9 +9,7 @@ const startup = config.disableAutoRegister
       console.error('[gateway] Vercel loadConfig failed:', err);
     });
 
-const handler = serverless(app);
-
 module.exports = async (req, res) => {
   await startup;
-  return handler(req, res);
+  return app(req, res);
 };
